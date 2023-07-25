8d4a340b69c2afd861f8917c7dff270dbab562f1
harismita-ragupathy <ragupathy.harismita@divum.in>
2023-07-25 09:54:22 +0530

    Completed

--- CRUD Operation-Javascript/table.js
+++ CRUD Operation-Javascript/table.js
@@ -36,10 +36,40 @@ function insertIntoTable(){
     document.getElementById("details").reset();    
     
 }
-function deleteIt(td){
+function deleteIt(button){
     if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement;
-        document.getElementById("t").deleteRow(row.rowIndex);
+        row = button.parentElement.parentElement;
+        document.getElementsByClassName("t")[0].deleteRow(row.rowIndex);
         resetForm();
     }
+}
+function edit(button){
+        var row=button.parentElement.parentElement;
+        var cells=row.children;
+        var fname=cells[0].innerHTML;
+        var lname=cells[1].innerHTML;
+        var no=cells[2].innerHTML;
+        var hobby=cells[3].innerHTML;
+        
+        cells[0].innerHTML='<input type="text" value="'+fname+'">';
+        cells[1].innerHTML='<input type="text" value="'+lname+'">';
+        cells[2].innerHTML='<input type="text" value="'+no+'">';
+        cells[3].innerHTML='<input type="text" value="'+hobby+'">';
+        cells[4].innerHTML='<button onclick = "save(this)"> Save </button>';
+}
+function save(button){
+    var row=button.parentElement.parentElement;
+    var cells=row.children;
+    var newFName=cells[0].firstChild.value;
+    var newLName=cells[1].firstChild.value;
+    var newFNo=cells[2].firstChild.value;
+    var newHobby=cells[3].firstChild.value;
+
+    cells[0].innerHTML=newFName;
+        cells[1].innerHTML=newLName;
+        cells[2].innerHTML=newFNo;
+        cells[3].innerHTML=newHobby;
+        cells[4].innerHTML='<button onclick = "edit(this)"> Edit </button> <button onclick= "deleteIt(this)">Delete</button>';
+    
+
 }
